62a7ee177f84785a233bc18589d5e0fc54a30b07
SAKSAUD7 <saud.k@themetakey.com>
2023-12-08 00:29:48 +0530

    commit it

--- mymetakeynft/src/App.js
+++ mymetakeynft/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { ethers } from 'ethers';
+import { useState } from 'react';
+
+
+
+ function App() {
+ const [, setAccount] =useState("");
+ const [data, setData] =useState([]);
+
+ const connect = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+ let res= await provider.send("eth_requestAccounts", []);
+setAccount(res[0]);
+getData(res[0]);
+};
+
+ const getData =() => {
+  const options = { method: "GET", headers: {Accept: 'application/json', 'x-api-key': '87e86ede6bcc4ee8ac2467a6158f8295'}};
+
+  fetch (
+  'https://api.opensea.io/api/v2/events/accounts/0x10daa9f4c0f985430fde4959adb2c791ef2ccf83?event_type=&limit=50', 
+  options
+ )
+
+  .then((response) => response.json())
+  .then((response) => {
+    setData(response);
+    console.log(response);
+    })
+    .catch((err) => console .error(err))};
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <p>(account)</p>
+     <button onClick={connect}>Connect</button>
+     
     </div>
   );
 }
 
 export default App;
+
